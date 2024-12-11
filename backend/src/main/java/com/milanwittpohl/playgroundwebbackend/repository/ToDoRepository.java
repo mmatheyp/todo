7d377583a75a98d946ffa880b9afaaaaea8af6c9
mmatheyp <57466188+mmatheyp@users.noreply.github.com>
2024-12-11 10:57:33 +0100

    Delete backend directory

--- backend/src/main/java/com/milanwittpohl/playgroundwebbackend/repository/ToDoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.milanwittpohl.playgroundwebbackend.repository;
-
-import com.milanwittpohl.playgroundwebbackend.data.ToDo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface ToDoRepository extends MongoRepository<ToDo, String> {}
